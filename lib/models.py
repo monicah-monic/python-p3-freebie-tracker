5702dff96f4be257887849927ec8e533d3ff3b2c
monicah-monic <monicahwanjiru205@gmail.com>
2025-05-26 02:00:41 +0300

    freebie migrations

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref, declarative_base
+
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -16,8 +16,19 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    freebies = relationship('Freebie', back_populates = 'company')
+
     def __repr__(self):
         return f'<Company {self.name}>'
+    
+    def freebie(self, dev,item_name, value):
+        return Freebie(item_name=item_name, value= value, dev = dev)
+
+    
+    @classmethod
+    def oldest_company(cls, session):
+        return session.query(cls).order_by(cls.founding_year.asc()).first()
+
 
 class Dev(Base):
     __tablename__ = 'devs'
@@ -25,5 +36,31 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    freebies = relationship('Freebie', back_populates = 'dev')
+
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+    def received_one(self, item_name):
+        return any(freebie.item_name == item_name for freebie in self.freebies)
+
+    
+
+class Freebie(Base):
+    __tablename__ = 'freebies'  
+
+    id = Column(Integer(), primary_key=True)
+    item_name =Column(String())
+    value = Column(Integer())
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+
+    company = relationship('Company', back_populates = 'freebies')
+    dev = relationship('Dev', back_populates= 'freebies')
+
+    def __repr__(self):
+        return f'<Freebie {self.item_name} ${self.value}>'
+    
+    def print_details(self):
+        return f"{self.dev.name} owns a {self.item_name} from {self.company.name}."
+
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
+from models import Company, Dev, Freebie
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-# Script goes here!
+engine = create_engine('sqlite:///freebies.db')
+
+freebie1 = Freebie(item_name = 'T-shirt', value =20, company_id= 1, dev_id=3)
+freebie2 = Freebie(item_name = 'Mug', value =4, company_id= 3, dev_id=2)
+freebie3 = Freebie(item_name = 'Umbrella', value =30, company_id= 8, dev_id=1)
+freebie4 = Freebie(item_name = 'Hoodie', value =50, company_id= 2, dev_id=7)
+
+Session = sessionmaker(bind=engine)  
+session = Session()  
+session.add_all([freebie1, freebie2, freebie3, freebie4])
+session.commit()
